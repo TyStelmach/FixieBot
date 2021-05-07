ef5f07d0fbb14520d5ae70dfcca27111adcc79f8
Ty Stelmach <ty@Tys-Mac-mini.local>
2021-05-07 00:35:29 -0400

    refactors main start script

--- src/index.js
+++ src/index.js
@@ -1,61 +1,42 @@
+require('dotenv').config({ path: "../.env" });
+
 const Snoowrap = require('snoowrap');
 const { CommentStream } = require('snoostorm');
-const path = require('path');
-const env = require('dotenv').config({ path: path.resolve(__dirname, '../.env') }).parsed;
-const equations = require('./lib/equation');
-const validations = require('./lib/validation');
-const writeReply = require('./lib/response');
+const message = require('./lib/messages');
 const logger = require("./lib/logger").Logger;
 
-const client = new Snoowrap({
-  userAgent: env.username,
-  clientId: env.clientId,
-  clientSecret: env.clientSecret,
-  username: env.username,
-  password: env.password
+const reddit = new Snoowrap({
+  userAgent: process.env.REDDIT_USER_AGENT,
+  clientId: process.env.CLIENT_Id,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
 });
 
-
-const parseComments = (client) => {
-  logger.info(`FixieBot was started`);
-  const botStartedTime = Date.now() / 1000;
-  const comments = new CommentStream(client, {
-    subreddit: "testingground4bots",
-    limit: 10,
-    pollTime: 2000,
+(() => {
+  logger.info(`${process.env.REDDIT_USER} was started...`);
+  const stream = new CommentStream(reddit, {
+    subreddit: process.env.SUBREDDIT,
+    results: 100,
   });
 
-  comments.on('item', comment => {
-    const {id, created_utc, body, author, locked, link_url} = comment;
-    const botWasInvoked = validations.checkForMention(body, env.username);
-    const commentedAfterStart = created_utc > botStartedTime;
+  let commentIds = [];
 
-    if (botWasInvoked && commentedAfterStart) {
-      logger.info(`FixieBot was called by /u/${author.name}`);
-      const commandWasPassed = validations.checkForCommand(body) ? true : false;
-      if (commandWasPassed) {
-        const drivetrains = equations.getRatios(body);
-        if (drivetrains.length > 0) {
-          let response;
+  stream.on('item', async comment => {
+    const wasSummoned = comment.body && comment.body.toLowerCase().includes(process.env.REDDIT_USER.toLowerCase()) ? true : false;
 
-          if (!locked) {
-            drivetrains.forEach(drivetrain => {
-              const replyIntro = writeReply.writeIntro(author.name);
-              const replyBody = writeReply.writeTechnicalInfo(drivetrain);
-              const replyOutro = writeReply.writeOutro();
-              response = `${replyIntro}\n ${replyBody}\n ${replyOutro}`;
-            });
+    if (wasSummoned) {
+      const reply = await message.getReply(comment, commentIds);
+      if (!reply) return;
 
-            comment.reply(response);
-            logger.info(`FixieBot replied to /u/${author.name} -- ${link_url}${id}`);
-          }
-        }
-      } else {
-        logger.info(`ERROR: /u/${author.name} did not list a command`);
-      }
+      comment.reply(reply)
+      .then(resp => {
+        const botReplyLink = `${comment.link_url}${comment.id}`;
+        logger.info(`${process.env.REDDIT_USER} replied to /u/${comment.author.name} -- ${botReplyLink}`);
+      })
+      .catch(err => {
+        console.log(err);
+      })
     }
   })
-}
-
-parseComments(client);
-
+})();
---
+++ src/lib/messages.js
@@ -0,0 +1,54 @@
+// messages.js
+const content = require('../config/content.json');
+const equations = require('./equations.js');
+const logger = require("./logger").Logger;
+
+const hasCommand = (body) => {
+  const regex = new RegExp(`(?<=ratio\\()(.+?)(?=\\))`, 'igm');
+  return body.match(regex);
+};
+
+const formatReply = (id, author, commandPassed) => {
+  const drivetrainValues = commandPassed.map(valueStr => {
+    const values = valueStr.split(', ');
+    return equations.calculateRatio(values)
+  });
+
+  const replyIntro = `${content.intro.greeting} /u/${author.name}, ${content.intro.phrase}`;
+  let replyBody = '';
+  const replyOutro = 'Outro...';
+
+  drivetrainValues.forEach(drivetrain => {
+    const { chainring, cog, basicGearRatio, singleSkidPatch, ambidexSkidPatch } = drivetrain;
+    const headingString = `**${content.technical.chainring}(${chainring}) => ${content.technical.cog}(${cog})**`;
+    const ratioString = `- *${content.technical.gearRatio}*: **${basicGearRatio}**`;
+    const singlePatchString = `- *${content.technical.skidPatches}*: **${singleSkidPatch}**`;
+    const ambidexPatchString = `- *${content.technical.ambiSkidPatches}*: **${ambidexSkidPatch}**`;
+
+    replyBody += `\n ${headingString} \n ${ratioString} \n ${singlePatchString} \n ${ambidexPatchString} \n ---`;
+  })
+
+  return `${replyIntro} \n ${replyBody} \n ${replyOutro}`;
+};
+
+module.exports = {
+  async getReply(comment, prevCommentIds = []) {
+    const {id, author, body, replies} = comment;
+    const commandPassed = hasCommand(body);
+    let message = null;
+    //TODO: Check to not reply to ourselves
+
+    if (commandPassed) {
+      const replyAuthors = await replies.fetchAll().map(reply => reply.author.name.toLowerCase());
+      if (!replyAuthors.includes(process.env.REDDIT_USER.toLowerCase())) {
+        // Only reply if we never commented
+        message = formatReply(id, author, commandPassed);
+        logger.info(`ERROR: ${process.env.REDDIT_USER} has replied to /u/${author.name} already -- ignoring comment.`);
+      }
+      logger.info(`${process.env.REDDIT_USER} summoned with the command ${commandPassed} by /u/${author.name}.`);
+    } else {
+      logger.info(`ERROR: ${process.env.REDDIT_USER} was summoned by /u/${author.name} without a command.`);
+    }
+    if (message) return message;
+  }
+}
